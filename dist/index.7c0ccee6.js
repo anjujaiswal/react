a63e79a6325a161946b1e165fe06efdbaf073ca1
unknown <jaiswalanju2019@gmail.com>
2024-01-03 19:15:41 +0530

    episode-2

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("h1", {
+    id: "headin"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, "child1"),
+    React.createElement("div", {
+        id: "child2"
+    }, "child2")
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map

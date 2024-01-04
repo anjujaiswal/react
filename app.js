761aaf1c49b03dfeb877ae163ca3b51a9d7bf69a
unknown <jaiswalanju2019@gmail.com>
2024-01-04 15:15:35 +0530

    episode-3

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const heading = React.createElement("h1",{id:"headin"}, [React.createElement("div",{id:"child1"},"child1"),
-React.createElement("div",{id:"child2"},"child2")]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// const  heading = React.createElement("h1",{id:"headin"}, [React.createElement("div",{id:"child1"},"child1"),
+// React.createElement("div",{id:"child2"},"child2")]);
+const Logo = () =>(
+    <div className="header">
+       
+        <img src= "./images/logo.jpg" alt="logo"/>
+        <input  name="myInput"/>
+         <h5>icon</h5>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Logo/>);
